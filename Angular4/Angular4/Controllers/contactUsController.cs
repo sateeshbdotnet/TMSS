f2d1c206857f5ffb616fae1fadb8aef130b63ae5
SATEESHBIYYALA\sateesh <sateesh0286@gmail.com>
2018-01-21 17:41:09 -0800

    Added ContactUsservice and ContractUsController

---
+++ Angular4/Angular4/Controllers/contactUsController.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+using Angular4.DAL;
+using Angular4.Models;
+
+namespace Angular4.Controllers
+
+{
+    
+    public class contactUsController : ApiController
+    {
+        private contactUsRepository _contractUsRepository;
+
+        
+        public contactUsController()
+        {
+            _contractUsRepository = new contactUsRepository(new MorgoniteEntities());
+        }
+        [HttpGet]
+        public IHttpActionResult getContractDetails()
+        {
+            List<contactUsModel> List = _contractUsRepository.GetContactDetails();
+            return Ok(List);
+        }
+
+    }
+}
---
+++ Angular4/Angular4/DAL/contactUsRepository.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using Angular4.Models;
+using Angular4.DAL;
+
+namespace Angular4.DAL
+{
+    public class contactUsRepository
+    {
+        private MorgoniteEntities _morgoniteContext;
+
+        public  contactUsRepository(MorgoniteEntities morgoniteEntities)
+        {
+            _morgoniteContext = morgoniteEntities;
+        }
+        public List<contactUsModel> GetContactDetails()
+        {
+            var contactDetails = _morgoniteContext.customerDetails.ToList();
+            return contactDetails.Select(d => new contactUsModel()
+            {
+                Name = d.name,
+                Address = d.address,
+                EmailId = d.emailId,
+                Message = d.message,
+                PhoneNumber = d.phoneNumber
+            }).ToList();
+
+            
+        }
+    }
+}
